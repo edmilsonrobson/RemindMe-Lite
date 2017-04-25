7d0afce119eb02ca77ca5ce96468ae3ada1b1e0a
Edmilson <edmilsonrobson@gmail.com>
2017-04-24 22:31:58 -0300

    1.0rc, fully working!

--- assets/js/remindmelite.js
+++ assets/js/remindmelite.js
@@ -3,6 +3,10 @@ $('#main-container ul').on("click", "li", function(){
     $(this).toggleClass("completed-li")
 });
 
+$('.fa-pencil-square-o').on("click", function(){
+    $('#new-task-input').fadeToggle(200);
+});
+
 $('#main-container ul').on("click", '.remove-button', function(event){
     event.stopPropagation();
     $(this).parent().fadeOut(200, function(){
@@ -17,7 +21,7 @@ $('#new-task-input').keydown(function(event){
             var todoText = $(this).val();
             $(this).val("");
 
-            $('#tasks-list').append('<li><span class="remove-button"><i class="fa fa-trash"></i></span> ' + todoText + '</li>');
+            $('#tasks-list').append('<li><span class="remove-button"><i class="fa fa-trash-o" aria-hidden="true"></i> </span><a href="#">' + todoText + '</a></li>');
         }
     }
-})
+});
